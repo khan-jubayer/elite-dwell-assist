4da3415be09d579839e6d58fe10a31808af5fd54
Khan Jubayer <144028577+khan-jubayer@users.noreply.github.com>
2024-11-25 23:38:55 +0600

    Delete elite-dwell-assist-server directory

--- elite-dwell-assist-server/utils.js
+++
@@ -1,9 +0,0 @@
-function generateUniqueId() {
-  const timestamp = new Date().getTime();
-  const random = Math.floor(Math.random() * 1000);
-  return `${timestamp}-${random}`;
-}
-
-module.exports = {
-  generateUniqueId,
-};
